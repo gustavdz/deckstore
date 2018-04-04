a51d944ad3f3ce6bd48880c042a6940350da551b
Gustavo Decker <gustavdz@gmail.com>
2018-04-04 15:46:04 -0500

    cambio de namespaces Admin

---
+++ app/Http/Controllers/admin/ProductController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Product;
+use App\ProductImage;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index(){
+        $products = Product::paginate(10);
+        return view('admin.products.index')->with(compact('products')); //listado
+
+    }
+
+    public function create(){
+        return view('admin.products.create'); //formulario de registro
+    }
+
+    public function store(Request $request){
+        //validar
+        $messages=[
+            'name.required'=> 'Es necesario ingresar un nombre para el producto.',
+            'name.min'=> 'El nombre del producto debe tener al menos 3 caracteres.',
+            'description.required'=> 'Es necesario ingresar una descripción para el producto.',
+            'description.max'=> 'La descripción del producto no puede exceder a los 200 caracteres.',
+            'price.required'=> 'Es necesario ingresar un precio para el producto.',
+            'price.numeric'=> 'Es necesario valores numéricos para el precio',
+            'price.min'=> 'El percio del producto no puede ser negativo.',
+        ];
+        $rules =[
+            'name'=> 'required|min:3',
+            'description' => 'required|max:200',
+            'price' => 'required|numeric|min:0'
+        ];
+        $this->validate($request,$rules,$messages);
+        //registrar el nuevo producto en la base de datos
+        //dd($request->all());
+        $product = new Product();
+        $product->name=$request->input('name');
+        $product->description=$request->input('description');
+        $product->price=$request->input('price');
+        $product->long_description=$request->input('long_description');
+        $product->save(); // insert del objeto
+
+        return redirect('/admin/products');
+    }
+
+    public function edit($id){
+
+        $product = Product::find($id);
+        return view('admin.products.edit')->with(compact('product')); //formulario de registro
+    }
+
+    public function update(Request $request,$id){
+        //validar
+        $messages=[
+            'name.required'=> 'Es necesario ingresar un nombre para el producto.',
+            'name.min'=> 'El nombre del producto debe tener al menos 3 caracteres.',
+            'description.required'=> 'Es necesario ingresar una descripción para el producto.',
+            'description.max'=> 'La descripción del producto no puede exceder a los 200 caracteres.',
+            'price.required'=> 'Es necesario ingresar un precio para el producto.',
+            'price.numeric'=> 'Es necesario valores numéricos para el precio',
+            'price.min'=> 'El percio del producto no puede ser negativo.',
+        ];
+        $rules =[
+            'name'=> 'required|min:3',
+            'description' => 'required|max:200',
+            'price' => 'required|numeric|min:0'
+        ];
+        $this->validate($request,$rules,$messages);
+        //actualizar la informacion del producto en la base de datos
+        //dd($request->all());
+        $product = Product::find($id);
+        $product->name=$request->input('name');
+        $product->description=$request->input('description');
+        $product->price=$request->input('price');
+        $product->long_description=$request->input('long_description');
+        $product->save(); // insert del objeto
+
+        return redirect('/admin/products');
+    }
+
+    public function destroy($id){
+        ProductImage::where('product_id', $id)->delete();
+
+        $product = Product::find($id);
+        $product->delete(); // delete del objeto
+
+        return back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ Route::get('/', 'TestController@welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/products/{id}', 'ProductController@show');
 
-Route::middleware(['auth','admin'])->prefix('admin')->group(function(){
+Route::middleware(['auth','admin'])->prefix('admin')->namespace('Admin')->group(function(){
     Route::get('/products', 'ProductController@index'); //listado
     Route::get('/products/create', 'ProductController@create'); //formulario de creacion
     Route::post('/products', 'ProductController@store'); //crear
