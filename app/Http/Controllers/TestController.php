d8f71b614e26a61b4d199ce362a2c036efcdf9f7
Gustavo Decker <gustavdz@gmail.com>
2018-04-04 16:10:54 -0500

    paginado en el index

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,7 @@ class TestController extends Controller
 {
     //
     public function welcome(){
-        $products = Product::all();
+        $products = Product::paginate(9);//all();
         return view('welcome')->with(compact('products'));
     }
 }
